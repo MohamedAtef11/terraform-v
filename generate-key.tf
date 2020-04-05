19b2369204222b974c4d217d6307bfd49cbb267b
Mohamed Atef <49479848+MohamedAtef11@users.noreply.github.com>
2020-04-05 17:48:57 +0200

    Update generate-key.tf

--- generate-key.tf
+++ generate-key.tf
@@ -3,8 +3,8 @@ resource "tls_private_key" "key" {
   rsa_bits  = 2048
 }
 
-resource "aws_secretsmanager_secret" "aws-secret-1" {
-  name = "aws-secret-1"
+resource "aws_secretsmanager_secret" "aws-secret-2" {
+  name = "aws-secret-2"
 }
 
 resource "aws_key_pair" "aws_Pkey" {
@@ -13,7 +13,7 @@ resource "aws_key_pair" "aws_Pkey" {
 }
 
 resource "aws_secretsmanager_secret_version" "test-secret" {
-  secret_id     = "${aws_secretsmanager_secret.aws-secret-1.id}"
+  secret_id     = "${aws_secretsmanager_secret.aws-secret-2.id}"
   secret_string = "${tls_private_key.key.private_key_pem}"
 
 }
