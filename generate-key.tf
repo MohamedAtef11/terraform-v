5094de4ca3f1648a5fc6491cfcce325fd0789910
Mohamed Atef <49479848+MohamedAtef11@users.noreply.github.com>
2020-04-05 19:30:10 +0200

    Update generate-key.tf

--- generate-key.tf
+++ generate-key.tf
@@ -17,14 +17,6 @@ resource "aws_secretsmanager_secret_version" "test-secret-1" {
   secret_string = "${tls_private_key.key.private_key_pem}"
 
 }
-# resource "aws_secretsmanager_secret" "secret-manager1" {
-#   name = "secret-manager1"
-# }
-
-# resource "aws_key_pair" "aws_Pkey" {
-#   key_name   = "aws_Pkey"
-#   public_key = "${tls_private_key.key.public_key_openssh}"
-# }
 
 resource "local_file" "ec2-private-key" {
   content = "${tls_private_key.key.private_key_pem}"
